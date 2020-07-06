5d66b90171561b63195633cd8d63ffb20e3d8113
Gabriel Soares <gabrielsoaresii@outlook.com>
2020-07-06 15:14:35 -0300

    Adding https redirection

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ const httpsOptions = {
 
 const publicPathClient = path.join(__dirname, 'client', "build");
 
+app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele
+  if (req.headers["x-forwarded-proto"] == "http") //Checa se o protocolo informado nos headers é HTTP
+      res.redirect(`https://${req.hostname}${req.url}`); //Redireciona pra HTTPS
+  else //Se a requisição já é HTTPS
+      next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado
+});
+
 app.use(express.static(publicPathClient));
 app.use('*', express.static(publicPathClient));
 

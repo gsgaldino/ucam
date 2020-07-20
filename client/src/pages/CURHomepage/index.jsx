ac08943f7b31a1504bc0b0158ca41065181df78a
Gabriel Soares <gabrielsoaresii@outlook.com>
2020-07-20 11:32:29 -0300

    Adding: CUR Homepage

--- client/src/pages/CURHomepage/index.jsx
+++ client/src/pages/CURHomepage/index.jsx
@@ -7,9 +7,86 @@ export default function CURHomepage(){
   const modulos = () => (
     <React.Fragment>
       <ol>
-          <p>
-            Lorem Ipsum
-          </p>
+          <li><strong>Construindo sua carreira e currículo</strong>
+            <ul>
+              <li>
+                <strong>Modulo currículo:</strong>
+                <p>como chamar a atenção dos entrevistadores;</p>
+              </li>
+
+              <li>
+                <strong>Módulo linkedin;</strong>
+                <p>como fazer o seu perfil trabalhar para você;</p>
+              </li>
+
+              <li>
+                <strong>Post:</strong>
+                <p>quais as vantagens em saber usar o linkedin? - blog da alura;</p>
+              </li>
+
+              <li>
+                <strong>Post;</strong>
+                <p>como fazer um perfil de alta qualidade no linkedin e atrair mais trabalhos? - blog da alura;</p>
+              </li>
+
+              <li>
+                <strong>Post;</strong>
+                <p>como fazer uma leitura do mercado com foco em oportunidades de trabalho? - blog da alura.</p>
+              </li>
+
+            </ul>
+          </li>
+
+          <li><strong>Marketing pessoal</strong>
+            <ul>
+              <li>
+                <strong>Módulo personal branding:</strong>
+                <p>entenda o que é e como você pode usá-la a favor da sua carreira;</p>
+              </li>
+
+              <li>
+                <strong>Post:</strong>
+                <p>por que você deve saber estruturar um bom briefing de marketing pessoal - blog da alura;</p>
+              </li>
+
+              <li>
+                <strong>Módulo networking:</strong>
+                <p>potencialize oportunidades;</p>
+              </li>
+
+              <li>
+                <strong>Post:</strong>
+                <p>descubra as principais habilidades para destacar a sua carreira - blog da alura.</p>
+              </li>
+
+            </ul>
+          </li>
+
+          <li>
+            <strong>Carreira no exterior</strong>
+            <ul>
+              <li>
+                <strong>Módulo inglês:</strong>
+                <p>candidate-se a vagas no exterior;</p>
+              </li>
+
+              <li>
+                <strong>Módulo espanhol:</strong>
+                <p>candidate-se a vagas no exterior;</p>
+              </li>
+
+              <li>
+                <strong>Site podcast;</strong>
+                <p>carreira sem fronteiras;</p>
+              </li>
+
+              <li>
+                <strong>Podcast;</strong>
+                <p>carreira de programação no exterior.</p>
+              </li>
+
+            </ul>
+          </li>
 
         </ol>
     </React.Fragment>
@@ -20,15 +97,15 @@ export default function CURHomepage(){
 
       <LandingPage
         titulo="Currículos"
-        formId="250e0624-0ad5-4154-993f-6ebd071ddf52"
+        formId="71100345-1faf-46d4-a026-9842a99bd9ca"
         linkMatricula="https://cursos.alura.com.br/compra/candido-mendes-curriculos"
         cargaHoraria="40 horas"
         inicio="Agosto/2020"
         modalidade="100% on-line"
         investimento="R$ 600,00"
-        objetivoDoCurso="Lorem Ipsum"
+        objetivoDoCurso="O curso tem como objetivo explorar os pontos fortes e tornar mais palpável o plano de ação do candidato. Com técnicas de construção de carreira, marketing pessoal e construção de currículos em outros idiomas, como Inglês e Espanhol, para quem deseja construir uma carreira no exterior."
         
-        paraQuemSeDestina="Lorem Ipsum"
+        paraQuemSeDestina="O curso é voltado para profissionais formados, que precisam iniciar sua carreira ou se recolocar no mercado mas desejam aprimorar seu currículo ou profissionais interessados em aprender sobre construção de currículos."
         modulos={modulos()}
       />
 
